0eaa92358749704c3e8e2cea9104da0df7f39ac5
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2018-01-28 02:13:01 +0100

    control

--- Assessments/Week4/control_flow.js
+++ Assessments/Week4/control_flow.js
@@ -8,40 +8,20 @@
 // and `false` if it is not
 
 function isEven(num) {
-<<<<<<< HEAD
     if (num % 2 === 0) {
       return true;
     } else {
       return false;
     }
   }
-=======
-  if (num % 2 === 0) {
-   return true;
- } else {
-   return false;
-  }
-}
->>>>>>> origin
 
 // 2. The function Number.isNaN(num) will return true if num is not a number
 // or false if it is.
 // Use Number.isNan to extend your `isEven` function by including an `else if` statement
 // that checks whether a num is a number and
 // console.log => "That is not a number", if it is not a number
-
 function isEvenV2(num) {
-<<<<<<< HEAD
-  if (num % 2 === 0 ) {
-    return true;
-  } else if (num % 2 > 0) {
-    return false;
-  } else if (isNaN(num)) {
-    console.log("That is not a number");
-  }
-}
-=======
-  if (Number.isNaN(num)) {
+if (Number.isNaN(num)) {
     return true;
   }
   else if (isNaN(num)) {
@@ -51,11 +31,6 @@ function isEvenV2(num) {
    return false;
  }
 }
-console.log(isEvenV2(NaN)); // true
-console.log(isEvenV2("I am a string")); // "That is not a number"
-console.log(isEvenV2(100)); // false
-
->>>>>>> origin
 // 3. Complete the switch statment in the `getLunchOrder` with the following
 // case conditions:
 // > "sandwich" - console.log => "Sure thing! One sandwich, coming up."
