09b0ab0e42c9faeaf9c0b11fa88ac2f20ea96163
Chioma <chioma.onyekpere.185617@unn.edu.ng>
2018-01-28 01:16:57 +0100

    low battery

--- Assessments/Week4/function.js
+++ Assessments/Week4/function.js
@@ -1,32 +1,45 @@
-/**
-  In this assessment, you will be creating new Javascript functions.
-  Make sure to read the directions to build the functions carefully.
-  You should use the below patterns to write your functions:
-  ```
-  function {{name_of_function}} {
+
+  //In this assessment, you will be creating new Javascript functions.
+  //Make sure to read the directions to build the functions carefully.
+  //You should use the below patterns to write your functions:
+  // ```
+  // function {{name_of_function}} {
     // Write your code here
   }
 
   OR
 
-  const {{name_of_function}} = () => {
-    // Write your code here
-  }
+//  const {{name_of_function}} = () => {
+    const letsBegin = () => {
+}
 
-  ```
-*/
+
 
 // 1. write a function that takes a number as a parameter and returns it as a
 // string
+function myString (num) {
+  return num.toString();
+}
 
 // 2. write a function that takes a number as a parameter and returns that
 // number subtracted by 1
+const subtract = (num) => {
+  num = Number(num);
+  return num -= 1;
+}
 
 // 3. write a function that takes a number as a parameter and returns that
 // number added by 1
+function add (num) {
+  num = Number(num);
+  return num += 1;
+}
 
 // 4. write a function that takes two number as its parameters and returns the
 // addition of those two numbers
+function numAdd (num) {
+  return num + num; 
+}
 
 // 5. write a function that takes a number as its parameter and returns the
 // squared of that number
