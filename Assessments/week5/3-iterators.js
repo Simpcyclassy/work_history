4ba2a16e67479ae6a45aca2ef4cbd5f152068a5c
Chioma <chioma.onyekpere.185617@unn.edu.ng>
2018-02-03 06:38:51 +0100

    week 5

--- Assessments/week5/3-iterators.js
+++ Assessments/week5/3-iterators.js
@@ -25,16 +25,24 @@ const week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 
 
 // 1. Using the `forEach` function complete the instructions below:
 // Console.log every day in the week array
-
+week.forEach(function(days) {
+   console.log(' ~ ' + days)
+});
 // 2. Using the `some` function complete the instructions below:
 // Check the week array for days that are longer than six letters
 
-const hasDaysLongerThanSixLetters = /** erase this then write your coode here*/
-
+const hasDaysLongerThanSixLetters = function(days) {
+  return days.length>6;
+}
+console.log(week.some(hasDaysLongerThanSixLetters));
 // 3. Using the `filter` function complete the instructions below:
 // Filter the weeks array for all the days that are longer than six letters
 
-const filteredDaysLongerThanSixLetters = /** erase this then write your coode here*/
+const filteredDaysLongerThanSixLetters = week.filter(function(days) {
+return days.length > 5;
+});
+console.log(days);
+
 
 // 4. Using the `map` function complete the instructions below:
 // create a new array that has all the days captialized
@@ -42,4 +50,7 @@ const filteredDaysLongerThanSixLetters = /** erase this then write your coode he
 // HINT - use `toUpperCase` function to captialize strings
 // https://www.w3schools.com/jsref/jsref_touppercase.asp
 
-const arrayOfCapitalizedDays = /** erase this then write your coode here*/
+const arrayOfCapitalizedDays = week.map(function(days) {
+return days.toUpperCase;
+});
+console.log(days);
